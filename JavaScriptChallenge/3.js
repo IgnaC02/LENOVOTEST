8b34535fa7e5939ee0833abd2d2b59f70914f072
Ignacioc <63696926+IgnaC02@users.noreply.github.com>
2021-11-08 22:44:37 -0300

    Challenge done

--- JavaScriptChallenge/3.js
+++ JavaScriptChallenge/3.js
@@ -4,4 +4,52 @@
 //  *  int: year
 
 // **  Methods:
-//  *  boolean: isOn() *  void: turnOn() *  void: turnOff() *  String: toString() [returns year, make, model and the engine displacement (cc)] * String: getMarketingData() [returns make, model and year]
+//  *  boolean: isOn() *
+// void: turnOn() *
+// void: turnOff() *
+// String: toString() [returns year, make, model and the engine displacement (cc)] *
+// String: getMarketingData() [returns make, model and year]
+
+// 4) Create a Bicycle object with only the make [String] , model [String] and year [int] attributes and no methods.
+// Instantiate it and print to the console its internal state calling the getMarketingData method from the Car object.
+
+// 3
+var Car = {
+  make: "Ford",
+  model: "Mustang",
+  turnedOn: false,
+  engineDisplacement: 1200,
+  year: 1977,
+
+  isOn: function () {
+    return (this.turnedOn = Car.turnedOn);
+  },
+
+  turnOn: function () {
+    this.turnedOn = true;
+    return this.turnedOn;
+  },
+
+  turnOff: function () {
+    this.turnOff = false;
+    return this.turnOff;
+  },
+
+  toString: function () {
+    return [this.year, this.make, this.model, this.engineDisplacement];
+  },
+
+  getMarketingData: function () {
+    return [this.year, this.make, this.model];
+  },
+};
+console.log(Car.toString());
+
+// 4
+var Bicycle = {
+  make: "Mongoose",
+  model: "Dolomite",
+  year: 2021,
+};
+var bicycle1 = Car.getMarketingData.bind(Bicycle);
+console.log(bicycle1());
